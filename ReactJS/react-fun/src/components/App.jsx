9dfd702e5507e0bf213efe5b3b28d75cafaaf70d
talal02 <i190727@nu.edu.pk>
2022-02-02 19:59:04 +0500

    ReactJS revision-1

---
+++ ReactJS/react-fun/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Creed from './Creed';
+
+const App = () => {
+  return (
+    <div>
+      <Creed />
+      <Creed />
+      <Creed />
+      <Creed />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/react-fun/src/components/Creed.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Creed = () => {
+  return (
+    <div>
+      <h1>Hello Sir...</h1>
+    </div>
+  );
+}
+
+export default Creed;
