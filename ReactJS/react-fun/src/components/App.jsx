5dd39c2ea2c9221a5c683666cacf8a965e06768a
talal02 <i190727@nu.edu.pk>
2022-02-04 20:00:09 +0500

    React Effects & State Done..

--- ReactJS/react-fun/src/components/App.jsx
+++ ReactJS/react-fun/src/components/App.jsx
@@ -1,17 +1,54 @@
-import React from 'react';
-import Test from './Test';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+
+  const [email, setEmail] = useState('');
+
+  let handleEmailBlur = e => {
+    console.log('Blur');
+  }
+
+  let handleEmailChange = e => {
+    setEmail(e.target.value);
+    console.log('Changed');
+  }
+
+  let handleEmailFocus = e => {
+    console.log('Focus');
+  }
+
+  let handleKeyDown = e => {
+    console.log('down');
+  }
+
+  let handleKeyUp = e => {
+    console.log('up');
+  }
+
+  let handleKeyPress = e => {
+    console.log('press');
+  }
+
+  let handleClick = e => {
+    e.preventDefault();
+    console.log('clicked');
+  }
+
   return (
-    <div>
-      <Test 
-        stringTest="Talal"
-        numTest={3.52}
-        boolTest={true}
-        undTest={undefined}
-        nullTest={null}
+    <form className="form-group container row col-6 mx-auto border p-3">
+      <input 
+        value={email}
+        onChange={handleEmailChange}
+        onFocus={handleEmailFocus}
+        onBlur={handleEmailBlur}
+        onKeyDown={handleKeyDown}
+        onKeyPress={handleKeyPress}
+        onKeyUp={handleKeyUp}
+        placeholder='I have a ton of events...'
+        className='form-control'
       />
-    </div>
+      <button className='btn btn-warning mt-3' onClick={handleClick}>Submit</button>
+    </form>
   );
 }
 
--- ReactJS/react-fun/src/components/Test.jsx
+++ ReactJS/react-fun/src/components/Test.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 const Test = props => {
   return(
     <div>
-      <h1>Testing String {props.stringTest}</h1>
       <h1>Testing Number {props.numTest}</h1>
-      <h1>Boolean Test {props.boolTest}</h1>
-      <h1>Undefined Test {props.undTest}</h1>
-      <h1>Null Test {props.nullTest}</h1>
     </div>
   );
 }
