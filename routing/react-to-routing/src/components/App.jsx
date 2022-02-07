f39d692646757f59fe481c23ca7218376cbd7e9f
talal02 <i190727@nu.edu.pk>
2022-02-07 23:49:09 +0500

    React Routing On The Way

---
+++ routing/react-to-routing/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App;
